18fdab401a33bdd864fc699598275dba3697a1ef
Sean Omeife <omeifesean@gmail.com>
2025-02-11 17:22:36 +0000

    First commit
    commiting changes to code in repo

---
+++ Chapter_01_Getting_Started_with_java/Dinosaur/src/dinosaur.java
@@ -0,0 +1,16 @@
+
+public class dinosaur {
+    public static void main(String[] args){
+        String name = "Sean";
+        String openingHours = "08:00";
+        String closingHours = "20:00";
+
+        String summary = "I would also like to mention that the park opens at " + openingHours + ", and closes at " + closingHours + " everyday.";
+
+        System.out.println("Welcome, " + name + " to Mesozoic Eden!");
+
+        System.out.println(summary);
+
+        System.out.println("Mesozoic Eden is safe and secure.");
+    }
+}
---
+++ Chapter_01_Getting_Started_with_java/HelloWorld/src/Ex1.java
@@ -0,0 +1,15 @@
+public class Ex1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mesozoic Eden!");
+        
+        String name = "Sean";
+        String position = "park manager";
+        String output = "My name is " + name + " and I want to be a " + position + " in Mesozoic Eden. ";
+
+        String openingHours = "08:00";
+        String closingHours = "20:00";
+        String time = "I would also like to mention that the park opens at " + openingHours + ", and closes at " + closingHours + " everyday." ;
+
+        System.out.println( output + time );
+    }
+}
