032e29c949eac56ad25cc3bdce2c9d6173274bb4
Sean Omeife <omeifesean@gmail.com>
2025-04-22 23:29:40 +0100

    completed some examples

---
+++ Chapter_03_Operators_and_Casting/tests/RelationalTest.java
@@ -0,0 +1,13 @@
+public class RelationalTest {
+    public static void main(String[] args) {
+        int x = 3, y = 4;
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(x > y);
+        System.out.println(x >= y);
+        System.out.println(x < y);
+        System.out.println(x <= y);
+        System.out.println( 3 + 4.0);
+        System.out.println( 4 == 4.0);
+    }
+}
