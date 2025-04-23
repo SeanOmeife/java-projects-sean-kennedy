d2e5fe88049d7a53b5598813f99097cd72d69b39
Sean Omeife <omeifesean@gmail.com>
2025-04-23 23:04:32 +0100

    completed some examples in chapter 3

---
+++ Chapter_03_Operators_and_Casting/tests/bitwiseAnd.java
@@ -0,0 +1,8 @@
+public class bitwiseAnd {
+    public static void main (String[] args){
+        boolean b1 = false, b2 = true;
+        boolean res = b1 & (b2=false);
+        System.out.println(b1 + " " + b2 + " " + res);
+    }
+    
+}
---
+++ Chapter_03_Operators_and_Casting/tests/bitwiseOr.java
@@ -0,0 +1,9 @@
+
+
+public class bitwiseOr {
+    public static void main(String[] args) {
+        boolean b1 = false, b2 = true;
+        boolean res = b2 | (b1=true);
+        System.out.println(b1 + " " + b2 + " " + res);
+    }
+}
